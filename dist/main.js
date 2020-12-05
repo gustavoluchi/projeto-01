15912b79d9aece8e20acce44ca5256fe1ea7e68a
gustavoluchi <gustavo.luchi@gmail.com>
2020-12-04 22:04:15 -0300

    corrigindo aria-label dentro da class

--- dist/main.js
+++ dist/main.js
@@ -2,7 +2,7 @@ const lista=document.querySelector("#lista"),adicionar=document.querySelector("#
         <input id="cb-${c}" type="checkbox" class="m-2 h-6 w-6"/>
         <label for="cb-${c}" class="hidden">confirmar a conclusão do conteúdo</label>
         <span>${b}</span>
-        <button class="block w-10 h-10 btnApagar hover:gray-500 group aria-label="deletar item" onclick="apagaLi(${c})" value='${arrayDeItems.length}'>
+        <button class="block w-10 h-10 btnApagar hover:gray-500 group" aria-label="deletar item" onclick="apagaLi(${c})" value='${arrayDeItems.length}'>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="fill-current">
          <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" class="h-6 w-6 fill-current text-yellow-600 group-hover:text-yellow-800" />
         </svg>
